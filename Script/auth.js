11849aad4ace5dcbf67ba0856bed9a0c0bde0609
ms223uh@lnu.se <ms223uh@lnu.se>
2015-12-09 15:06:09 +0000

    authClass is done, for this time

---
+++ Script/auth.js
@@ -0,0 +1,39 @@
+var auth = {
+    
+    CLIENT_ID: "551653508701-vcoujiksgh3bpij3qk5t6ai2a2vj4hep.apps.googleusercontent.com",
+    SCOPES:['https://mail.google.com/'],
+    
+    checkAuth:function() {
+    gapi.auth.auth(
+      {
+        'client_id':auth.CLIENT_ID,
+        'scope': auth.SCOPES.join(' '),
+        'immediate': true
+      }, auth.handleAuthResult);
+  },
+
+ 
+  handleAuthResult:function(authResult) {
+    var authDiv = document.getElementById('authorize-div');
+    if (authResult && !authResult.error)
+    {
+      
+      authDiv.style.display = 'none';
+      
+    } 
+    else 
+    {
+      
+      authDiv.style.display = 'inline';
+    }
+  },
+
+  handleAuthLogin:function(event) {
+    gapi.auth.authorize(
+      {client_id: auth.CLIENT_ID, scope: auth.SCOPES, immediate: false, authuser:""},
+      auth.handleAuthResult);
+    return false;
+  }
+
+
+}
